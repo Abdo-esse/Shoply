eb84878538fa97cc7407b08c71d05f2e437682be
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-12 12:47:19 +0100

    J'ai coriege quelle que retouche dans le  filtre

--- assets/script_product/fetchData.js
+++ assets/script_product/fetchData.js
@@ -53,21 +53,3 @@ function allCategories(products) {
   }
 }
 
-// // Event listener for filtering categories
-// var filtrage = document.getElementById("catégorie");
-// filtrage.addEventListener("change", filterByCategory);
-
-// // Function to filter products by category
-// function filterByCategory(e) {
-//   if (e.target.value === "All" ||e.target.value === "catégoriem" ) {
-//     displayCategories(myProducts); // Show all products if 'All' is selected
-//   } else {
-//     const filteredProducts = myProducts.filter(product => product.catégorie === e.target.value);
-//     displayCategories(filteredProducts); // Show filtered products
-//   }
-// }
-
-// // Function to simulate adding product to cart (you can implement this function as needed)
-// function addToCart(productId) {
-//   console.log(`Product with ID ${productId} added to cart.`);
-// }
